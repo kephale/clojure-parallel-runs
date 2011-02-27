f191c24289ddfb2c9d988d66474c987617ae0843
Kyle Harrington <kyleh@cs.brandeis.edu>
2011-02-27 13:19:59 -0500

    Updating project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clojure-parallel-runs "1.0.0-SNAPSHOT"
+(defproject clojure-parallel-runs "1.0.1-SNAPSHOT"
   :description "Simple job distribution through RabbitMQ"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [rabbitcj "0.1.0-SNAPSHOT"]]
+                 [rabbitcj "0.1.1-kephaleSNAPSHOT"]]
   :main clojure-parallel-runs.core)
